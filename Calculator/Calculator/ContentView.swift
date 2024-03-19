b25d09360c00e93d1a4c13c6645abd7bf80e0f73
Hyun Jaeyeon <jen@Hyunui-MacBookPro.local>
2024-03-19 21:47:04 +0900

    ♻️refactor: enum 생성 후 buttonDisplayName 추가

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,16 +7,64 @@
 
 import SwiftUI
 
+enum ButtonType: String {
+    case first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, zero
+    case comma, equal, plus, minus, multiply, divide
+    case percent, opposite, clear
+    
+    var buttonDisplayName: String {
+        switch self {
+            case .first:
+                return "1"
+            case .second:
+                return "2"
+            case .third:
+                return "3"
+            case .fourth:
+                return "4"
+            case .fifth:
+                return "5"
+            case .sixth:
+                return "6"
+            case .seventh:
+                return "7"
+            case .eighth:
+                return "8"
+            case .nineth:
+                return "9"
+        case .zero:
+            return "0"
+        case .comma:
+               return "."
+           case .equal:
+               return "="
+           case .plus:
+               return "+"
+           case .minus:
+               return "-"
+           case .multiply:
+               return "*"
+           case .divide:
+               return "/"
+           case .percent:
+               return "%"
+           case .opposite:
+               return "±"
+           case .clear:
+               return "C"
+        }
+    }
+}
 struct ContentView: View {
     
     @State private var totalNumber: String = "0"
     
-    private let buttonData: [[String]] = [
-        ["C", "/", "%", "/"],
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", "0", ".", "="]
+    private let buttonData: [[ButtonType]] = [
+        [.clear, .opposite,.percent, .divide],
+        [.seventh, .eighth, .nineth, .multiply],
+        [.fourth, .fifth, .sixth, .minus],
+        [.first, .second, .third, .plus],
+        [.zero, .zero, .comma, .equal]
     ]
     
     var body: some View {
@@ -41,15 +89,15 @@ struct ContentView: View {
                         ForEach(line, id: \.self){ item in
                             Button{
                                 if totalNumber == "0"{
-                                    totalNumber = item
+                                    totalNumber = "7"
                                 } else {
-                                    totalNumber += item
+                                    totalNumber += "7"
                                 }
                             } label: {
-                                Text(item)
+                                Text(item.buttonDisplayName)
                                     .frame(width: 80,
                                            height: 80)
-                                    .background(.gray)
+                                    .background(Color.gray)
                                     .cornerRadius(40)
                                     .foregroundColor(.white)
                                     .font(.system(size: 33))
